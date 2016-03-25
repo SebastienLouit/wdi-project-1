27678f71990445bb89a417b71bdc2393ca218f37
SebastienLouit <sebastien.louit@gmail.com>
2016-03-25 11:20:30 +0000

    HTMLstructure ok and color cascading ok

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log ("cool")
+$(function(){
+
+
+
+
+  $( "#startButton" ).click(function() {
+    $( "li" ).each(function( index, element ) {
+      // element == this
+      $( element ).css( "backgroundColor", "yellow" );
+      if ( $( this ).is( "#li4" ) ) {
+        return false;
+      }
+    });
+  });
+
+  })
+
+///COPY-PASTING AREA//
+
