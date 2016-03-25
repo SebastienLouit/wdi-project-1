08dc5845770b6d19af20bdc5ba5bbd1b342ab46b
SebastienLouit <sebastien.louit@gmail.com>
2016-03-25 13:54:47 +0000

    Box randomly disapearing on the srceen

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 $(function(){
 
-
-
-
-  $( "#startButton" ).click(function() {
-    $( "li" ).each(function( index, element ) {
-      // element == this
-      $( element ).css( "backgroundColor", "yellow" );
-      if ( $( this ).is( "#li4" ) ) {
-        return false;
-      }
+  // var $randomBox = $("li").indexOf([Math.floor(Math.random()*24)])
+ 
+ 
+  $(function(){
+    $("#startButton").on("click",function(){
+      var element = $("#li"+[Math.floor(Math.random()*24)]);
+      element.hide("slow",function(){
+        setTimeout(function(){
+          element.show();
+        },1000)
+      });
     });
-  });
-
   })
+          
 
-///COPY-PASTING AREA//
+
+})
+
+
+
+
+
+
 
